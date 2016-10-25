abfdc6118a9c750648746551c4d305903d5a8ebe
Jeff Brunson <jeff.a.brunson@gmail.com>
2016-10-25 12:34:07 -0600

    add animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { library, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+
   render() {
     const { titleStyles } = styles;
     const { id, title } = this.props.library;
@@ -18,6 +35,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -31,4 +49,10 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
